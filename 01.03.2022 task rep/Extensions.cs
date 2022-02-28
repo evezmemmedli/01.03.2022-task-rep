500238dab6205ec1845d48dc86f0840e930ec304
evezmemmedli <avazkm@code.edu.az>
2022-02-28 21:42:39 +0400

    Add project files.

---
+++ 01.03.2022 task rep/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._03._2022_task_rep
+{
+    static class Extensions
+    {
+        public static string isCapitalized(this string word)
+        {
+            #region
+            word = word.ToLower();
+            var newWord = word.Split(' ');
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (string item in newWord)
+            {
+                stringBuilder.Append(char.ToUpper(item[0])); /*+item.Substring(1) + " ";*/
+                for (int i = 1; i < item.Length; i++)
+                {
+                    stringBuilder.Append(item[i]);
+
+                }
+                stringBuilder.Append(" ");
+            }
+            return stringBuilder.ToString();
+
+            #endregion
+        }
+    }
+}
---
+++ 01.03.2022 task rep/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01._03._2022_task_rep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "exTensiON claSSi staTIc OLMalIdiR";
+            Console.WriteLine(word.isCapitalized());
+        }
+    }
+}
